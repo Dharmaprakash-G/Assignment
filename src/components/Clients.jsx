a773e9c03640d33f8ceceb0e0e96b77866f062a5
Dharmaprakash-G <prakash.ganesan26@gmail.com>
2024-12-21 15:11:37 +0530

    i comment out line 8 in clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () => {
   return (
     <div className='bg-white p-10'>
         <div className='font-montserrat text-4xl flex items-center justify-center'>
-        Our Trusted Clients
+        {/* Our Trusted Clients */}
         </div>
         <div className='flex items-center justify-center mt-20'>
             <img src={Frame} />
